7c22b5405397c71ff0cf47e7421270c6714c5744
Javier Vazquez <javier@RadioRodriguezWork.local>
2010-11-03 17:14:12 +0100

    updated config file

--- php-optimizer/config/config.inc.php
+++ php-optimizer/config/config.inc.php
@@ -1,7 +1,7 @@
 <?php
  	//Base Build Dirs
- 	define('BASE_BUILD_PATH', dirname(__FILE__).'/../build');
- 	define('BASE_PROJECT', 'DigiMag_Base');
+ 	define('BASE_BUILD_PATH', dirname(__FILE__).'/../..');
+ 	define('BASE_PROJECT', 'magazine.optimizer');
  	define('BASE_PROJECT_PATH', BASE_BUILD_PATH.'/'.BASE_PROJECT);
- 	define('BASE_MAGAZINE_CONTENT_PATH', BASE_PROJECT_PATH.'/magazine');
+ 	define('BASE_MAGAZINE_CONTENT_PATH', BASE_PROJECT_PATH.'');
 	define('OPTIMISE_IMAGE_CMD', 'php '.dirname(__FILE__).'/../validatePackage.php');
