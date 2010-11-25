4f325ec0501fb6aad8bdd1f1d648f6170a1b2631
Javier Vazquez <javier@RadioRodriguezWork.local>
2010-11-25 11:28:31 +0100

    cleaned up app directory

--- php-optimizer/config/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
- 	//Base Build Dirs
- 	define('BASE_BUILD_PATH', dirname(__FILE__).'/../..');
- 	define('BASE_PROJECT', 'magazine.optimizer');
- 	define('BASE_PROJECT_PATH', BASE_BUILD_PATH.'/'.BASE_PROJECT);
- 	define('BASE_MAGAZINE_CONTENT_PATH', BASE_PROJECT_PATH.'');
-	define('OPTIMISE_IMAGE_CMD', 'php '.dirname(__FILE__).'/../validatePackage.php');
